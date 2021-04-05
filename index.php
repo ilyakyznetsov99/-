dc6a118130c0086cb7c52221b3fd7b00a6eec5e3
ilyakyznetsov99 <81756136+ilyakyznetsov99@users.noreply.github.com>
2021-04-05 22:00:08 +0300

    Add files via upload

--- index.php
+++ index.php
@@ -3,50 +3,37 @@
 
 <script>
 function getdetails(){
-	tyear={ 'data' : String(document.getElementById("year").value)};
-    var year =JSON.stringify(tyear);
-    //alert(year);
-
-   var t= JSON.stringify(year);
-  if (document.getElementById("vivod")	) {document.getElementById("vivod").remove(); var a=document.getElementsByTagName("p");
-  			a[0].remove();}
-
-        var text;
-        console.log(fetch("DB_moves",{method:"PUT",body:t, headers: {'Content-Type': 'application/json;charset=utf-8'}}).then(response=> {return response.json()} ));
-
-        	text=fetch("DB_moves",{method:"PUT",body: year}).then(response=> {return response.json()} );
-    
-        	 text.then(function(text){  
-        	let div = document.createElement('p');
-        				div.Id="usings";
-        		div.innerHTML="<H1>  Пользователи "+text.length+"<H1>";
-        	
+if (document.getElementById("output")) {
+//после нажатия на кнопку дописывались новые теги , засоряя страницу, потому здесь и проверка на существованиеэтих элементов
+	document.getElementById("output").remove(); 
+	var a=document.getElementsByTagName("p");
+  	a[0].remove();
+  }
+	year= JSON.stringify({ 'data' : String(document.getElementById("year").value)});//переменная года,конвертируемая в json формат
+        var json_data;
+        	json_data=fetch("DB_moves",{method:"PUT",body: year}).then(response=> {return response.json()} );
+        	 json_data.then(function(json_data){  
+        	let div = document.createElement('p');//создание тега куда вносится количество пользователей
+        		div.Id="count_users";
+        		div.innerHTML="<H1>  Пользователи "+json_data.length+"<H1>";
         		var table = document.createElement("table");
-        		table.id="vivod";
+        		table.id="output";
         		document.body.append(div);
-        		
-        		for (let i = 0; i < text.length; i++) { 
-  				
-  				let  cellnum=0;
+        		for (let i = 0; i < json_data.length; i++) { 
   				if (i==0) {
-  						console.log(cellnum);
-  						var tr=table.insertRow(0);
-  						for(key in text[i]){
-  						var tr1=tr.insertCell();
-  						tr1.innerHTML=key;
+  						var head_of_table=table.insertRow(0);
+  						for(key in json_data[i]){
+  						var cells_of_head=head_of_table.insertCell();
+  						cells_of_head.innerHTML=key;
   						}
   					}
-  		
-  				var tr=table.insertRow(i+1);
-  				for(key in text[i]){
-  					let r=cellnum;
-  					var tr1=tr.insertCell();
-  						tr1.innerHTML = text[i][key];
-  					cellnum++;
+  				var body_off_table=table.insertRow(i+1);
+  				for(key in json_data[i]){
+  					var cells_of_body=body_off_table.insertCell();
+  						cells_of_body.innerHTML = json_data[i][key];
   				};
   				
 }
-
 document.body.append(table);
 });
 
