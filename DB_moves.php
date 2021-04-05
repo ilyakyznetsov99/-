ba6daf4422ab80adbbaa8be758e06143a9703353
ilyakyznetsov99 <81756136+ilyakyznetsov99@users.noreply.github.com>
2021-04-05 20:46:29 +0300

    Add files via upload

--- DB_moves.php
+++ DB_moves.php
@@ -1,33 +1,26 @@
 <?php 
+ini_set('display_errors', 0);
+ini_set('display_startup_errors', 0);
+error_reporting(E_ALL);
 $host="localhost";
 $db="fp";
 $user="root";
 $passwd="";
-$pdo=new pdo('mysql:host=localhost;dbname=fp;charset=utf8',$user,$passwd);//подключение
-
- if (isset($_POST["fyear"])) {
-    $year=$_POST["fyear"];
+$pdo=new pdo('mysql:host=localhost;dbname=fp;charset=utf8',$user,$passwd);
+$content = file_get_contents('php://input');
+$cont=json_decode($content,1);
+    $year=$cont['data'];
     $yearfrst=$year."-01-01";
     $yearsec=$year."-12-31";
+    
     global $pdo;
     $stmt=$pdo->prepare("SELECT * FROM `users` WHERE `bdate` between ? and ?");
 
        $stmt->execute(array( $yearfrst,$yearsec ));
 
-       $row=$stmt->fetchAll();
-       echo "<H1>  Пользователи(".count($row).")";
-    echo "<table name="."tbl"."><tr><th>Id</th><th>first_name</th><th>last_name</th><th>bdate</th></tr>";
- foreach ($row as $row1) {
-    
-         $rows = count($row1)/2;
-
-         echo "</tr>";
-        for ($i=0; $i < $rows; ++$i) { 
-            echo "<td>$row1[$i]</td>";
-        }
-    }
-    echo "</table>";
-   }
-
+       $row=$stmt->fetchAll(PDO::FETCH_ASSOC);
+       header('Content-Type: application/json'); 
+         echo json_encode($row, JSON_PRETTY_PRINT);
+  
 
 ?>
--- index.php
+++ index.php
@@ -1,23 +1,62 @@
 <html>
 <head>
-<script src="https://code.jquery.com/jquery-3.5.0.js"></script>
+
 <script>
 function getdetails(){
-    var year = $('#year').val();
-    $.ajax({
-        type: "POST",
-        url: "DB_moves.php",
-        data: {fyear:year}
-    }).done(function( result )
-        {
-            $("#msg").html( result );
-        });
+	tyear={ 'data' : String(document.getElementById("year").value)};
+    var year =JSON.stringify(tyear);
+    //alert(year);
+
+   var t= JSON.stringify(year);
+  if (document.getElementById("vivod")	) {document.getElementById("vivod").remove(); var a=document.getElementsByTagName("p");
+  			a[0].remove();}
+
+        var text;
+        console.log(fetch("DB_moves",{method:"PUT",body:t, headers: {'Content-Type': 'application/json;charset=utf-8'}}).then(response=> {return response.json()} ));
+
+        	text=fetch("DB_moves",{method:"PUT",body: year}).then(response=> {return response.json()} );
+    
+        	 text.then(function(text){  
+        	let div = document.createElement('p');
+        				div.Id="usings";
+        		div.innerHTML="<H1>  Пользователи "+text.length+"<H1>";
+        	
+        		var table = document.createElement("table");
+        		table.id="vivod";
+        		document.body.append(div);
+        		
+        		for (let i = 0; i < text.length; i++) { 
+  				
+  				let  cellnum=0;
+  				if (i==0) {
+  						console.log(cellnum);
+  						var tr=table.insertRow(0);
+  						for(key in text[i]){
+  						var tr1=tr.insertCell();
+  						tr1.innerHTML=key;
+  						}
+  					}
+  		
+  				var tr=table.insertRow(i+1);
+  				for(key in text[i]){
+  					let r=cellnum;
+  					var tr1=tr.insertCell();
+  						tr1.innerHTML = text[i][key];
+  					cellnum++;
+  				};
+  				
+}
+
+document.body.append(table);
+});
+
 }
 </script>
 </head>
 <body>
                <input type="text" name="year" id="year" />
                <input type="submit" name="submit" id="submit" value="submit" onClick = "getdetails()" />
+               <h1 id="usnum"> </h1>
     <div id="msg"></div>
 </body>
 </html>
