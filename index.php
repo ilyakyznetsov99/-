6eb0e75349bb5afc5f713d5d225c44382eafa5bf
ilyakyznetsov99 <81756136+ilyakyznetsov99@users.noreply.github.com>
2021-04-06 18:42:28 +0300

    Add files via upload

--- index.php
+++ index.php
@@ -3,39 +3,33 @@
 
 <script>
 function getdetails(){
-if (document.getElementById("output")) {
-//после нажатия на кнопку дописывались новые теги , засоряя страницу, потому здесь и проверка на существованиеэтих элементов
-	document.getElementById("output").remove(); 
-	var a=document.getElementsByTagName("p");
-  	a[0].remove();
-  }
-	year= JSON.stringify({ 'data' : String(document.getElementById("year").value)});//переменная года,конвертируемая в json формат
+	year= JSON.stringify({ 'data' : String(document.getElementById("year").value)});
         var json_data;
         	json_data=fetch("DB_moves",{method:"PUT",body: year}).then(response=> {return response.json()} );
         	 json_data.then(function(json_data){  
-        	let div = document.createElement('p');//создание тега куда вносится количество пользователей
-        		div.Id="count_users";
-        		div.innerHTML="<H1>  Пользователи "+json_data.length+"<H1>";
-        		var table = document.createElement("table");
-        		table.id="output";
-        		document.body.append(div);
+        		if(json_data.length>0){
+        		html=[];
+        		html.push("<H1>  Пользователи "+json_data.length+"<H1>");
+        		html.push("<table> <tr>")
         		for (let i = 0; i < json_data.length; i++) { 
   				if (i==0) {
-  						var head_of_table=table.insertRow(0);
   						for(key in json_data[i]){
-  						var cells_of_head=head_of_table.insertCell();
-  						cells_of_head.innerHTML=key;
+  						html.push("<td>"+key+"</td>");
   						}
+  						html.push("</tr>");
   					}
-  				var body_off_table=table.insertRow(i+1);
+  				html.push("<tr>");
   				for(key in json_data[i])
   				{
-  					var cells_of_body=body_off_table.insertCell();
-  						cells_of_body.innerHTML = json_data[i][key];
+  						html.push( "<td>"+json_data[i][key]+"</td>");
   				};
+  				html.push("</tr>");
   				
-}
-document.body.append(table);
+			}
+			html.push("</table>");
+			
+			document.getElementById("output").innerHTML=html.join('');
+		}
 });
 
 }
@@ -45,6 +39,6 @@ document.body.append(table);
                <input type="text" name="year" id="year" />
                <input type="submit" name="submit" id="submit" value="submit" onClick = "getdetails()" />
                <h1 id="usnum"> </h1>
-    <div id="msg"></div>
+    <div id="output" class="output"></div>
 </body>
 </html>
